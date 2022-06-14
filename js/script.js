5e085a2272bfccab615201aff96d14f134ffaec5
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-06-14 23:13:16 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
-// Copyright (c) 2022 Brayden Blank All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: June 2022
+// Created by: Brayden Blank
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
-"use strict"
+'use strict'
 
+/**
+ * Check service worker.
+ */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit-6-04-HTML/sw.js", {
     scope: "/ICS2O-Unit-6-04-HTML/",
   })
 }
 
+/**
+ * This function calculates the volume of a sphere
+ */
 window.onload = function() {
-  // this calculates volume of a sphere
-
   const params = new URLSearchParams(document.location.search)
 
-  // input
-  const circleRadius = params.get('r')
-  console.log(circleRadius)
-
-  // process
-  const volume = (4 / 3) * Math.Pi * (circleRadius**3)
-  const dimensions = "<ul>\n<li>Radius = " + circleRadius + "</li>\n<ul>"
+  // input and process
+  const radius = params.get('r')
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  const dimensions = "Radius: " + radius + " cm"
 
   // output
-  document.getElementById('dimensions').innerHTML = dimensions
-  document.getElementById('volume-of-sphere').innerHTML = 'Volume is: ' + volume + ' cm³!' 
+  if (radius > 0) {
+    document.getElementById('dimensions').innerHTML = dimensions
+    document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+  } else {
+    document.getElementById('volume').innerHTML = 'Volume is NaN.'
+  }
 }
